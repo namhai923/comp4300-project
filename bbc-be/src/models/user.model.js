fd62da208d2b6bc6191fa5ab543d6514a691c13d
Hai Nguyen <namhainguyenminh@gmail.com>
2024-03-07 14:40:09 -0600

    backend clean up

--- bbc-be/src/models/user.model.js
+++ bbc-be/src/models/user.model.js
@@ -1,269 +1,119 @@
-const mongoose = require("mongoose");
-const uuid = require("uuid");
-
-const Decimal128 = mongoose.Decimal128;
-
-let summaryElementSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Decimal128,
-    default: 0,
-  },
-});
-
-let summarySchema = new mongoose.Schema({
-  total: {
-    type: Decimal128,
-    required: true,
-  },
-  summary: {
-    type: [summaryElementSchema],
-    required: true,
-  },
-});
-
-let favorSchema = new mongoose.Schema(
-  {
-    favorId: {
-      type: String,
-      required: true,
-    },
-    userName: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Decimal128,
-      required: true,
-    },
-    description: {
-      type: String,
-      maxlength: 100,
-    },
-    accepted: {
-      type: Boolean,
-    },
-  },
-  { timestamps: true }
-);
-
-let repaySchema = new mongoose.Schema(
-  {
-    repayId: {
-      type: String,
-      required: true,
-    },
-    userName: {
-      type: String,
-      required: true,
-    },
-    amount: {
-      type: Decimal128,
-      required: true,
-    },
-    description: {
-      type: String,
-      maxlength: 100,
-    },
-    accepted: {
-      type: Boolean,
-    },
-    send: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+const mongoose = require('mongoose');
+const uuid = require('uuid');
 
 let message = new mongoose.Schema(
-  {
-    message: {
-      type: String,
-      required: true,
+    {
+        message: {
+            type: String,
+            required: true
+        },
+        sending: {
+            type: Boolean,
+            required: true
+        }
     },
-    sending: {
-      type: Boolean,
-      required: true,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 let conversation = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  latestMessage: {
-    type: message,
-  },
-  unRead: {
-    type: Number,
-    default: 0,
-    required: true,
-  },
-  messages: {
-    type: [message],
-    default: [],
-  },
+    userName: {
+        type: String,
+        required: true
+    },
+    latestMessage: {
+        type: message
+    },
+    unRead: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    messages: {
+        type: [message],
+        default: []
+    }
 });
 
 let contactSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
+    userName: {
+        type: String,
+        required: true
+    }
 });
 
 let pokeSchema = new mongoose.Schema({
-  pokemon: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Pokemon",
-  },
+    pokemon: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Pokemon'
+    }
 });
 
 let notificationSchema = new mongoose.Schema(
-  {
-    notificationId: {
-      type: String,
-      default: uuid.v4,
+    {
+        notificationId: {
+            type: String,
+            default: uuid.v4
+        },
+        userName: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            required: true
+        },
+        read: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
     },
-    userName: {
-      type: String,
-      required: true,
-    },
-    type: {
-      type: String,
-      required: true,
-    },
-    read: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 let userSchema = new mongoose.Schema(
-  {
-    userName: {
-      type: String,
-      required: true,
-      unique: true,
+    {
+        userName: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        firstName: {
+            type: String,
+            required: true
+        },
+        lastName: {
+            type: String,
+            required: true
+        },
+        dob: {
+            type: Date
+        },
+        phoneNumber: {
+            type: String
+        },
+        creditPoints: {
+            type: Number,
+            default: 100
+        },
+        contacts: {
+            type: [contactSchema],
+            default: []
+        },
+        conversationHistory: {
+            type: [conversation],
+            default: []
+        },
+        notificationList: {
+            type: [notificationSchema],
+            default: []
+        },
+        pokeCollection: {
+            type: [pokeSchema],
+            default: []
+        }
     },
-    firstName: {
-      type: String,
-      required: true,
-    },
-    lastName: {
-      type: String,
-      required: true,
-    },
-    dob: {
-      type: Date,
-    },
-    phoneNumber: {
-      type: String,
-    },
-    creditPoints: {
-      type: Number,
-      default: 100,
-    },
-    favorSummary: {
-      type: summarySchema,
-      default: { total: 0, summary: [] },
-    },
-    debtSummary: {
-      type: summarySchema,
-      default: { total: 0, summary: [] },
-    },
-    pendingFavor: {
-      type: [favorSchema],
-      default: [],
-    },
-    pendingRepay: {
-      type: [repaySchema],
-      default: [],
-    },
-    favorHistory: {
-      type: [favorSchema],
-      default: [],
-    },
-    debtHistory: {
-      type: [favorSchema],
-      default: [],
-    },
-    repayHistory: {
-      type: [repaySchema],
-      default: [],
-    },
-    contacts: {
-      type: [contactSchema],
-      default: [],
-    },
-    conversationHistory: {
-      type: [conversation],
-      default: [],
-    },
-    notificationList: {
-      type: [notificationSchema],
-      default: [],
-    },
-    pokeCollection: {
-      type: [pokeSchema],
-      default: [],
-    },
-  },
-  { collection: "Users" }
+    { collection: 'Users' }
 );
 
-favorSchema.set("toJSON", {
-  getters: true,
-  transform: (doc, ret) => {
-    if (ret.amount) {
-      ret.amount = parseFloat(ret.amount);
-    }
-    delete ret._id;
-    return ret;
-  },
-});
-
-repaySchema.set("toJSON", {
-  getters: true,
-  transform: (doc, ret) => {
-    if (ret.amount) {
-      ret.amount = parseFloat(ret.amount);
-    }
-    delete ret._id;
-    return ret;
-  },
-});
-
-summaryElementSchema.set("toJSON", {
-  getters: true,
-  transform: (doc, ret) => {
-    if (ret.amount) {
-      ret.amount = parseFloat(ret.amount);
-    }
-    delete ret._id;
-    return ret;
-  },
-});
-
-summarySchema.set("toJSON", {
-  getters: true,
-  transform: (doc, ret) => {
-    if (ret.total) {
-      ret.total = parseFloat(ret.total);
-    }
-    delete ret._id;
-    return ret;
-  },
-});
-
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
--- bbc-be/src/validators.js
+++ bbc-be/src/validators.js
@@ -1,119 +1,93 @@
-const Joi = require("joi");
-const phoneNumberJoi = Joi.extend(require("joi-phone-number"));
-const { POKEMON_TYPES } = require("./controllers/pokegene/pokeData");
-Joi.objectId = require("joi-objectid")(Joi);
+const Joi = require('joi');
+const phoneNumberJoi = Joi.extend(require('joi-phone-number'));
+const { POKEMON_TYPES } = require('./controllers/pokegene/pokeData');
+Joi.objectId = require('joi-objectid')(Joi);
 
-const validator = (schema) => (payload) =>
-  schema.validate(payload, { abortEarly: false });
+const validator = (schema) => (payload) => schema.validate(payload, { abortEarly: false });
 
 const userNameSchema = Joi.object({
-  userName: Joi.string().email().max(50).required(),
+    userName: Joi.string().email().max(50).required()
 });
 
 const UUIDSchema = Joi.object({
-  id: Joi.string()
-    .guid({
-      version: ["uuidv4"],
-    })
-    .required(),
+    id: Joi.string()
+        .guid({
+            version: ['uuidv4']
+        })
+        .required()
 });
 
 const registerSchema = Joi.object({
-  userName: Joi.string().email().max(50).required(),
-  password: Joi.string().min(8).max(50).required(),
-  firstName: Joi.string()
-    .max(50)
-    .pattern(new RegExp(/^[aA-zZs]+$/))
-    .required(),
-  lastName: Joi.string()
-    .max(50)
-    .pattern(new RegExp(/^[aA-zZs]+$/))
-    .required(),
+    userName: Joi.string().email().max(50).required(),
+    password: Joi.string().min(8).max(50).required(),
+    firstName: Joi.string()
+        .max(50)
+        .pattern(new RegExp(/^[aA-zZs]+$/))
+        .required(),
+    lastName: Joi.string()
+        .max(50)
+        .pattern(new RegExp(/^[aA-zZs]+$/))
+        .required()
 });
 
 const loginSchema = Joi.object({
-  userName: Joi.string().email().max(50).required(),
-  password: Joi.string().min(8).max(50).required(),
+    userName: Joi.string().email().max(50).required(),
+    password: Joi.string().min(8).max(50).required()
 });
 
 const searchUserSchema = Joi.object({
-  searchQuery: Joi.string().required(),
-});
-
-const makeRequestSchema = Joi.object({
-  accounts: Joi.array()
-    .min(1)
-    .unique()
-    .items(Joi.string().email().max(50))
-    .required(),
-  amount: Joi.number().min(1).required(),
-  description: Joi.string().allow(null, "").max(100),
-});
-
-const responseSchema = Joi.object({
-  accepted: Joi.boolean().required(),
-  id: Joi.string()
-    .guid({
-      version: ["uuidv4"],
-    })
-    .required(),
+    searchQuery: Joi.string().required()
 });
 
 const updateInfoSchema = Joi.object({
-  firstName: Joi.string()
-    .max(50)
-    .pattern(new RegExp(/^[aA-zZs]+$/))
-    .required(),
-  lastName: Joi.string()
-    .max(50)
-    .pattern(new RegExp(/^[aA-zZs]+$/))
-    .required(),
-  dob: Joi.date().max(new Date()).allow(""),
-  phoneNumber: phoneNumberJoi.string().phoneNumber().allow(""),
+    firstName: Joi.string()
+        .max(50)
+        .pattern(new RegExp(/^[aA-zZs]+$/))
+        .required(),
+    lastName: Joi.string()
+        .max(50)
+        .pattern(new RegExp(/^[aA-zZs]+$/))
+        .required(),
+    dob: Joi.date().max(new Date()).allow(''),
+    phoneNumber: phoneNumberJoi.string().phoneNumber().allow('')
 });
 
 const removeContactsSchema = Joi.object({
-  removeContacts: Joi.array()
-    .min(1)
-    .unique()
-    .items(Joi.string().email().max(50))
-    .required(),
+    removeContacts: Joi.array().min(1).unique().items(Joi.string().email().max(50)).required()
 });
 
 const sendMessageSchema = Joi.object({
-  userName: Joi.string().email().max(50).required(),
-  message: Joi.string(),
+    userName: Joi.string().email().max(50).required(),
+    message: Joi.string()
 });
 
 const generatePokemonSchema = Joi.object({
-  types: Joi.array()
-    .min(1)
-    .max(2)
-    .items(Joi.string().valid(...POKEMON_TYPES))
-    .required(),
+    types: Joi.array()
+        .min(1)
+        .max(2)
+        .items(Joi.string().valid(...POKEMON_TYPES))
+        .required()
 });
 
 const removePokemonSchema = Joi.object({
-  removeId: Joi.objectId().required(),
+    removeId: Joi.objectId().required()
 });
 
 const sendPokemonSchema = Joi.object({
-  sendId: Joi.objectId().required(),
-  userName: Joi.string().email().max(50).required(),
+    sendId: Joi.objectId().required(),
+    userName: Joi.string().email().max(50).required()
 });
 
 module.exports = {
-  validateUserName: validator(userNameSchema),
-  validateUUID: validator(UUIDSchema),
-  validateRegister: validator(registerSchema),
-  validateLogin: validator(loginSchema),
-  validateSearchUser: validator(searchUserSchema),
-  validateMakeRequest: validator(makeRequestSchema),
-  validateResponse: validator(responseSchema),
-  validateUpdateInfo: validator(updateInfoSchema),
-  validateRemoveContacts: validator(removeContactsSchema),
-  validateSendMessage: validator(sendMessageSchema),
-  validateGeneratePokemon: validator(generatePokemonSchema),
-  validateRemovePokemon: validator(removePokemonSchema),
-  validateSendPokemon: validator(sendPokemonSchema),
+    validateUserName: validator(userNameSchema),
+    validateUUID: validator(UUIDSchema),
+    validateRegister: validator(registerSchema),
+    validateLogin: validator(loginSchema),
+    validateSearchUser: validator(searchUserSchema),
+    validateUpdateInfo: validator(updateInfoSchema),
+    validateRemoveContacts: validator(removeContactsSchema),
+    validateSendMessage: validator(sendMessageSchema),
+    validateGeneratePokemon: validator(generatePokemonSchema),
+    validateRemovePokemon: validator(removePokemonSchema),
+    validateSendPokemon: validator(sendPokemonSchema)
 };
